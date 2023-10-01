e0fe78cfaca60d96b9a5c240d436574bd7ead279
siva <mangalisivapullaiah@gmail.com>
2023-10-01 08:59:46 +0530

    3 List products

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'ELON MUSK',
+      slug: 'elon-musk',
+      category: 'books',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'none',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Biography of Elon Musk',
+    },
+    {
+      name: 'DEEP WORK.',
+      slug: 'deep work',
+      category: 'books',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'none',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'Insights about work.',
+    },
+    {
+      name: 'RICH DAD POOR DAD',
+      slug: 'rich-dad-poor-dad',
+      category: 'books',
+      image: '/images/p3.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'none',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Real life emotions.',
+    },
+    {
+      name: 'The Philosophy of money',
+      slug: 'the-philosophy-of-money',
+      category: 'books',
+      image: '/images/p4.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'none',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'Valley of money',
+    },
+  ],
+};
+export default data;
